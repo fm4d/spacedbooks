023b5ed7c36c7f1818fd080cec471e3de56368e0
fm4d <m4dolaf@gmail.com>
2021-04-18 18:14:16 +0200

    remove config

--- config.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-SPACED_REPETITION_INTERVALS = [30, 60, 120, 180, 360, 360, 360, 360, 360, 360, 360, 360]
-
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-LOGGER.addHandler(logging.FileHandler('main.log'))
